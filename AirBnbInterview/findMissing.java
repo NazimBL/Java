22c15df53cfdcbbdfcabb22523a27be485492290
Bellabaci Nazim <30374106+NazimBL@users.noreply.github.com>
2019-07-26 04:09:39 +0200

    Update findMissing.java

--- AirBnbInterview/findMissing.java
+++ AirBnbInterview/findMissing.java
@@ -3,9 +3,21 @@ public static void main(String[] args) {
     int a[]={4,6,47,12,85};
     int b[]={4,47,85,12};
      
-    System.out.println(findMissing(a,b));
+    System.out.println(findMissingXor(a,b));
     }
     
+    public static int findMissingXor(int a[], int b[]){
+    
+   
+    int xor_sum=0;
+   
+    for(int i=0;i<a.length;i++)xor_sum^=a[i];
+    for(int i=0;i<b.length;i++)xor_sum^=b[i];
+               
+    return xor_sum;
+    
+    }
+
     public static int findMissing(int a[], int b[]){
     
     boolean tag=false;
